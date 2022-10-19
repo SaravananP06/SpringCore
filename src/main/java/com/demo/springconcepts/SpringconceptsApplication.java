74609c1b0c2cb4273d01ee6416be316ccb21e99a
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-10-20 02:14:40 +0530

    [Saravanan]: Strated with project

---
+++ src/main/java/com/demo/springconcepts/SpringconceptsApplication.java
@@ -0,0 +1,14 @@
+package com.demo.springconcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringconceptsApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to spring boot project");
+		SpringApplication.run(SpringconceptsApplication.class, args);
+	}
+
+}
